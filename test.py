f18f5221ee6c46cef0ea5266d89c546d11e1d427
sharadhi S S <60086120+sharadhirao@users.noreply.github.com>
2022-09-10 08:11:45 +0530

    Update test.py

--- test.py
+++ test.py
@@ -7,6 +7,12 @@ with open('/mnt/vol/build-info.yaml', 'r') as file:
 with open('/mnt/vol/config.json', 'w') as json_file:
     json.dump(configuration, json_file)
     
+with open('/mnt/vol/helm-command.yaml', 'r') as file:
+    configuration = yaml.safe_load(file)
+
+with open('/mnt/vol/config2.json', 'w') as json_file:
+    json.dump(configuration, json_file)
+    
 output = json.dumps(json.load(open('/mnt/vol/config.json')), indent=2)
 print(output)
 
