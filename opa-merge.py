49e88e28049122a919511457294e94967b09d302
sharadhi S S <60086120+sharadhirao@users.noreply.github.com>
2022-10-03 18:56:56 +0530

    Update opa-merge.py

--- opa-merge.py
+++ opa-merge.py
@@ -2,21 +2,21 @@ import json
 
 file_name = open('/mnt/vol/rego-org-proj-policy.json')
 print(file_name)
-data = json.loads(file_name.read())
+# data = json.loads(file_name.read())
 
-input_file_name = open('/mnt/vol/input.json')
-input_data = json.loads(input_file_name .read())
-environment = input_data["environment"]
+# input_file_name = open('/mnt/vol/input.json')
+# input_data = json.loads(input_file_name .read())
+# environment = input_data["environment"]
 
-proj_level = data["proj_rbac"]["allow"]
-org_level = data["org_rbac"]["allow"]
+# proj_level = data["proj_rbac"]["allow"]
+# org_level = data["org_rbac"]["allow"]
 
-if proj_level == False and org_level == False:
-    print("Failed")
-    exit(1)
-elif proj_level == True and org_level == False:
-    if environment == "dev" or environment == "QA":
-        print("Some scans in organization level for",environment,"environment did not passed")
-        for key in input_data:
-            if input_data[key] == "SKIPPED":
-                print(key)
+# if proj_level == False and org_level == False:
+#     print("Failed")
+#     exit(1)
+# elif proj_level == True and org_level == False:
+#     if environment == "dev" or environment == "QA":
+#         print("Some scans in organization level for",environment,"environment did not passed")
+#         for key in input_data:
+#             if input_data[key] == "SKIPPED":
+#                 print(key)
