c3a568c4a33e074adda9f60227fdca728f7b2d62
sharadhi S S <60086120+sharadhirao@users.noreply.github.com>
2022-10-04 14:28:59 +0530

    Update opa-merge.py

--- opa-merge.py
+++ opa-merge.py
@@ -1,5 +1,6 @@
 import json
 # open opa result file and load to variable
-file_name = open('/mnt/vol/test.json')
+file_name = open('/mnt/vol/rego-org-proj-policy.json','r')
 data = json.loads(file_name.read())
 print(data)
+proj_level = data["proj_rbac"]["allow"]
