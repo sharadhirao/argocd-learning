9d710250ba25ed84048023c61226759bcf5e4375
sharadhi S S <60086120+sharadhirao@users.noreply.github.com>
2022-10-04 14:08:07 +0530

    Update opa-merge.py

--- opa-merge.py
+++ opa-merge.py
@@ -1,5 +1,5 @@
 import json
 # open opa result file and load to variable
-file_name = open('/mnt/vol/rego-org-proj-policy.json')
+file_name = open('/mnt/vol/test.json')
 data = json.loads(file_name.read())
 print(data)
