1a99168f91a0fee72b9708a7c663fb3831db84ad
sharadhi S S <60086120+sharadhirao@users.noreply.github.com>
2022-10-04 13:16:43 +0530

    Update opa-merge.py

--- opa-merge.py
+++ opa-merge.py
@@ -8,24 +8,3 @@ print("contents: ", contents)
 
 res = json.loads(contents)
 print(type(res), res)
-
-# file_name = open('/mnt/vol/rego-org-proj-policy.json')
-# data = json.loads(file_name.read())
-# print(data)
-# input_file_name = open('/mnt/vol/input.json')
-# input_data = json.loads(input_file_name .read())
-# print(input_data)
-# environment = input_data["environment"]
-
-# proj_level = data["proj_rbac"]["allow"]
-# org_level = data["org_rbac"]["allow"]
-
-# if proj_level == False and org_level == False:
-#     print("Failed")
-#     exit(1)
-# elif proj_level == True and org_level == False:
-#     if environment == "dev" or environment == "QA":
-#         print("Some scans in organization level for",environment,"environment did not passed")
-#         for key in input_data:
-#             if input_data[key] == "SKIPPED":
-#                 print(key)
